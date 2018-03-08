60c56c8486bc5f3e1d305f6f2a842f9cc5f8d12d
Will Larry <will23larry@gmail.com>
2018-03-08 13:23:58 -0500

    added state and props to todo

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import React, { Component } from 'react';
 import './App.css';
-import ToDo from './components/ToDo.js'
+import ToDo from './components/ToDo'
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      todos: [
+        { description: 'Walk the dog', isCompleted: true},
+        { description: 'Pick up milk', isCompleted: true},
+        { description: 'Take kid to doctor', isCompleted: true},
+      ]
+    }
+  }
+
   render() {
     return (
       <div className="App">
         <ul>
-          <ToDo />
-          <ToDo />
+          { this.state.todos.map( (todo, index) =>
+            <ToDo key={index} description={todo.description} isCompleted={todo.isCompleted}/>
+          )}
         </ul>
       </div>
     );
